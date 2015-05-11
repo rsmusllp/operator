c8601300862ff7f1c973ce922624e4bb4834075a
Spencer McIntyre <zeroSteiner@gmail.com>
2015-05-11 14:15:59 -0400

    Add a pylintrc file and fix style in accordance w/PEP8

--- main.py
+++ main.py
@@ -1,64 +1,54 @@
 #!/usr/bin/env python
 
-__version__ = "0.1"
+__version__ = '0.1'
 
-from os import chdir, getcwd, mkdir
-from os.path import exists
-from time import strftime
+import logging
+import os
+import time
 
 from kivy import platform
 from kivy.app import App
 from kivy.core.audio import SoundLoader
-from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.textinput import TextInput
-from kivy.uix.button import Button
 
-android = False
+on_android = False
 
-if platform =='android':
+if platform == 'android':
 	from plyer import camera
- 	android = True
-
-
+	on_android = True
 
 class Operator(BoxLayout):
+	def __init__(self, *args, **kwargs):
+		self.logger = logging.getLogger("kivy.operator.{0}".format(self.__class__.__name__))
+		super(Operator, self).__init__(*args, **kwargs)
 
 	def do_play_sound(self, sound_file):
 		# load a sound and play it
 		sound = SoundLoader.load(sound_file)
 		sound.play()
-		return
 
 	def do_take_picture(self):
 		# take a picture and save it
-		if android == False:
-			print('Camera is not implemented on this platform')
-		else:
-			picture_name = str(strftime('%Y-%m-%d-%H-%M-%S')) + '.jpg'
-			picture_output = '/storage/sdcard0/' + picture_name
-			try:
-				camera.take_picture(picture_output, self.verify_image_saved(picture_output))
-			except:
-				raise
+		if not on_android:
+			self.logger.warning('Camera is not implemented on this platform')
+			return
+		picture_name = time.strftime('%Y-%m-%d-%H-%M-%S') + '.jpg'
+		picture_output = '/storage/sdcard0/' + picture_name
+		camera.take_picture(picture_output, self.verify_image_saved(picture_output))
 
 	def verify_image_saved(self, file_name):
 		# check that the image was saved correctly
-		success = exists(file_name)
-		return success
-
-
+		return os.path.exists(file_name)
 
 class MainApp(App):
-    def build(self):
-        operator = Operator()
-        return operator
+	def build(self):
+		return Operator()
 
-    def on_pause(self):
-    	return True
+	def on_pause(self):
+		return True
 
-    def on_resume(self):
-    	pass
+	def on_resume(self):
+		pass
 
 if __name__ == '__main__':
-    MainApp().run()
+	MainApp().run()
